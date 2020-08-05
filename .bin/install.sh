02462f98c62a1331c61e695ca2bdd829397a0375
watr <watr@users.noreply.github.com>
2020-08-06 08:50:48 +0900

    add .gitignore_global and install script

--- .bin/install.sh
+++ .bin/install.sh
@@ -4,3 +4,6 @@ script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")"; pwd)"
 dot_dir="$(cd "${script_dir}"; cd ..; pwd)"
 
 command ln -snf "${dot_dir}/.zshrc" "${HOME}"
+
+command ln -snf "${dot_dir}/.gitignore_global" "${HOME}"
+git config --global core.excludesfile ~/.gitignore_global
