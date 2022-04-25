b65f59db36f97f0dc6ae67c9784629b0163ce035
HASHIMOTO Wataru <watr@users.noreply.github.com>
2022-04-25 23:06:25 +0900

    update global git ignore config file path

--- .bin/install.sh
+++ .bin/install.sh
@@ -5,5 +5,9 @@ dot_dir="$(cd "${script_dir}"; cd ..; pwd)"
 
 command ln -snf "${dot_dir}/.zshrc" "${HOME}"
 
-command ln -snf "${dot_dir}/.gitignore_global" "${HOME}"
-git config --global core.excludesfile ~/.gitignore_global
+# git directory
+git_dir="${HOME}/.config/git"
+if [ ! -d "${git_dir}" ]; then
+    mkdir -p "${git_dir}"
+fi
+command ln -snf "${dot_dir}/.gitignore_global" "${git_dir}/ignore"
